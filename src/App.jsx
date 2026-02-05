74f0e782b41a0164d5ca09c313b8b55604dc5a9e
edpoli <edpoli96@gmail.com>
2026-02-05 16:59:29 +0100

    delete messages

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,7 @@ import './App.css'
 
   function App () {
          const [chatMessages, setChatMessages ] =  useState([{  
-          message:"hello chatbot",
-          sender :"user",
-          id: 'id1'
-
-        },
-        {
-            message :"Hello! How can I help you?",
-            sender : "robot",
-            id: 'id2'
-          },
-
-        {
-          message:"Can you get me today's date?", 
-          sender:"user",
-          id: 'id3'
-        },
-
-        {
-          message: "2nd February",
-          sender:"robot",
-          id: 'id4'
+          
         }]);
         
         // const [chatMessages, setChatMessages] = array; // array destructuring
